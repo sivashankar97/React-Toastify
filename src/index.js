8cc2461a7bf8de26e0ee3e7d7bedd4f1356b688b
sivashankar97 <hexshankar1997@gmail.com>
2023-03-08 18:54:41 +0530

    refactor(Toastify): restyle the autoclose in toastify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ToastContainer, toast } from "react-toastify";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// default minimum style for autoClose inside
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  state = {
+    border: true
+  };
+
+  toggleBorder = () => this.setState({ border: !this.state.border });
+
+  render() {
+    const { border } = this.state;
+    return (
+      <div className="App">
+        <button
+          onClick={() =>
+            toast("Disapear after a few sec", {
+              autoClose: 1000,
+              ...(border && { className: "border" })
+            })
+          }
+        >
+          Show notification
+        </button>
+
+        <button onClick={this.toggleBorder}>
+          {border ? "Hide" : "Show"} border
+        </button>
+        <ToastContainer autoClose={1000} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
