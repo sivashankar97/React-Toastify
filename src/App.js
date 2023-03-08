19a4714bc7d560bcfd1cfd8178df5bbb4097f330
sivashankar97 <hexshankar1997@gmail.com>
2023-03-08 11:47:00 +0530

    refactor(Toast): create without css file in toastify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
-function App() {
+export default function App(){
+  const notify = () => toast("Wow so easy !",{autoClose:1000});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={notify}>Notify !</button>
+      <ToastContainer 
+      autoClose={1000}/>
     </div>
   );
 }
-
-export default App;
